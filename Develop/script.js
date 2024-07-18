34dac89fffabc974b5604254bd2b40c32e9d81f3
Fin M <qbres333@hotmail.com>
2024-07-17 19:44:09 -0600

    create constants

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,31 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
+  // create an empty object for employees
+  const employeeList = {};
+
+  // create an empty employee array  
+  const empArray = [];
+  
+  const firstName = window.prompt("Enter the employee's first name:");
+  const lastName = window.prompt("Enter the employee's last name:");
+  const salary = window.prompt("Enter the employee's salary (no commas):");
+
+  //click add employee button (event listener)
+  addEmployeesBtn.addEventListener('click')
+
+  // if(addEmployeesBtn) {
+    
+  //   if( (xxx == null) || (xxx == undefined) ) {
+  //     window.alert("Invalid employee name. Try again.");
+
+  //   }
+  // }
+
+  //ask for first name
+  //ask for last name
+  //ask for salary
+
 }
 
 // Display the average salary
