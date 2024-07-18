a13a0d82827d7c6665fe5a7ed01cdcac6068be23
Fin M <qbres333@hotmail.com>
2024-07-18 12:25:52 -0600

    calculate average

--- Develop/script.js
+++ Develop/script.js
@@ -100,7 +100,15 @@ const collectEmployees = function() {
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
-  
+  let sum = 0;
+  const average = 0;
+
+  for (let i = 0; i < employeesArray.salary.length; i++) {
+    sum = sum + employeesArray.salary[i];
+    average = sum/employeesArray.salary.length;
+  }
+  return average;
+
 }
 
 // Select a random employee
